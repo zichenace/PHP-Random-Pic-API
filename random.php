19686ebd6a7f6662391754d54ad1c80a5d908215
Texas <79037972+zichenace@users.noreply.github.com>
2022-02-02 21:33:58 +0800

    Add files via upload

--- random.php
+++ random.php
@@ -2,7 +2,7 @@
 
 /*
 By:梓宸
-WEB:https://www.zichen.zone/
+WEB:https://zichen.zone/
 */
 
 $APIname='ZiC API';
@@ -12,12 +12,12 @@ $image_file = 'img.txt';
 if(!file_exists($image_file)) {
 	die($image_file.'数据文件不存在');
 }
+
 $data = file_get_contents($image_file);
 $data = explode(PHP_EOL, $data);
 $randKey = rand(0, count($data));
 $imgurl = $data[$randKey];
 $imgurl = str_replace(array("/r","/n","/r/n",), '', $imgurl);
-
 
 //输出
 $returnType = $_GET['get'];
@@ -26,6 +26,12 @@ switch ($returnType) {
 		$json['API_name'] = $APIname;
 		$json['id'] = $randKey;
 		$json['imgurl'] = $imgurl;
+		//检测图片状态码
+		#$code = curl_init ($imgurl);
+		#curl_setopt ($code,CURLOPT_RETURNTRANSFER,1);
+		#curl_exec ($code);
+		#$httpcode = curl_getinfo ( $code, CURLINFO_HTTP_CODE );
+		#curl_close ($code);
 		$imageInfo = getimagesize($imgurl);
 		$json['width'] = "$imageInfo[0]";
 		$json['height'] = "$imageInfo[1]";
@@ -37,5 +43,6 @@ switch ($returnType) {
 		header("Location:" . $imgurl);
 	break;
 }
+
 
 ?>
