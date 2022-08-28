8d149fd581cc4bff04eba98cdd273047a41d2967
Texas <79037972+zichenace@users.noreply.github.com>
2022-08-28 19:10:19 +0800

    Update random.php

--- random.php
+++ random.php
@@ -21,10 +21,10 @@ $data = file_get_contents($image_file);
 $data = explode(PHP_EOL, $data);
 $randKey = rand(0, count($data));
 $imgurl = $data[$randKey];
-$imgurl = str_replace(array("/r","/n","/r/n",), '', $imgurl);
+$imgurl = str_replace(array("\r","\n","\r\n",), '', $imgurl);
 
 //输出API内容
-$returnType = $_GET['get'];
+$returnType = $_GET['return'];
 switch ($returnType) {
 
 	default:				//默认情况下，直接返回链接
@@ -35,12 +35,14 @@ switch ($returnType) {
 		$json['id'] = $randKey;
 		$json['imgurl'] = $imgurl;
 
-		//检测图片状态码（按需开启）
-		#$code = curl_init ($imgurl);
-		#curl_setopt ($code,CURLOPT_RETURNTRANSFER,1);
-		#curl_exec ($code);
-		#$httpcode = curl_getinfo ( $code, CURLINFO_HTTP_CODE );
-		#curl_close ($code);
+		/*
+		#检测图片状态码（按需开启）
+		$code = curl_init ($imgurl);
+		curl_setopt ($code,CURLOPT_RETURNTRANSFER,1);
+		curl_exec ($code);
+		$httpcode = curl_getinfo ( $code, CURLINFO_HTTP_CODE );
+		curl_close ($code);
+		*/
 
 		$imageInfo = getimagesize($imgurl);
 		$json['width'] = "$imageInfo[0]";
